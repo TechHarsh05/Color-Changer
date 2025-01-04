741f91a36a160d66c048755531fea6f6cdfdee55
Harsh <techharsh05@gmail.com>
2025-01-05 03:10:34 +0530

    Left button fixed

--- script.js
+++ script.js
@@ -30,8 +30,18 @@ window.addEventListener('keydown', (event) => {
            else if(tside === 70 && !(lside < 15) && lside <= 55 ){} 
            else if(tside === 70 && !(lside < 65) && lside <= 75 ){} 
             else{ tside = tside + move; }}
-
-        if(lside!==0 && event.key === "ArrowLeft"){lside = lside - move;}
+            
+            if(lside!==0 && event.key === "ArrowLeft"){
+            if(lside === 15 && !(tside < 20) && tside <= 35 ){}
+            else if(lside === 15 && !(tside < 45) && tside <= 70 ){}
+            else if(lside === 50 && !(tside < 40) && tside <= 45 ){}
+            else if(lside === 60 && !(tside < 75) && tside <= 85 ){}
+            else if(lside === 65 && !(tside < 45) && tside <= 50 ){}
+            else if(lside === 75 && !(tside < 25) && tside <= 35 ){}
+            else if(lside === 75 && !(tside < 55) && tside <= 65 ){}
+            else if(lside === 90 && !(tside < 5) && tside <= 25 ){}
+            else if(lside === 90 && !(tside < 35) && tside <= 85 ){}
+            else{lside = lside - move;}}
         if(lside!==90 && event.key === "ArrowRight"){lside = lside + move;}
         
     box.style.left = lside + "%";
