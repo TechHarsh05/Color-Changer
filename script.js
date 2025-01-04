ff451fba2e9acc2e95d941fe6a2781c87a1d50c9
Harsh <techharsh05@gmail.com>
2025-01-05 02:46:13 +0530

    bottom button fixed

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ let right = document.getElementById('right')
 
 let lside = 50;
 let tside = 0;
-window.addEventListener('keydown', (event) => {
-    let move = 5;
+let move = 5;
 
-        if(tside!==0 && event.key === "ArrowUp"){tside = tside - move;}
-        if(tside!==90 && event.key === "ArrowDown"){tside = tside + move;}
+window.addEventListener('keydown', (event) => {
+        if(tside!==0 && event.key === "ArrowUp"){
+            {tside = tside - move;}
+        }
+        if(tside!==90 && event.key === "ArrowDown"){
+           if(tside === 0 && !(lside < 5) && lside <= 85 ){}
+           else if(tside === 20 && !(lside < 20) && lside <= 70 ){} 
+           else if(tside === 30 && !(lside < 80) && lside <= 85 ){} 
+           else if(tside === 40 && !(lside < 5) && lside <= 10 ){} 
+           else if(tside === 40 && !(lside < 55) && lside <= 60 ){} 
+           else if(tside === 50 && !(lside < 20) && lside <= 70 ){} 
+           else if(tside === 70 && !(lside < 15) && lside <= 55 ){} 
+           else if(tside === 70 && !(lside < 65) && lside <= 75 ){} 
+            else{ tside = tside + move; }}
+            
         if(lside!==0 && event.key === "ArrowLeft"){lside = lside - move;}
         if(lside!==90 && event.key === "ArrowRight"){lside = lside + move;}
         
@@ -18,4 +30,6 @@ window.addEventListener('keydown', (event) => {
     box.style.top = tside + "%";
 
 })
+
+
 
